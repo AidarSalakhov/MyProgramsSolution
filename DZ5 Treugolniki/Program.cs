04263cd5f0c4a8399c310a24029b32382661a9ed
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-06 16:49:17 +0300

    Просмотрено видео, начал ДЗ

---
+++ DZ5 Treugolniki/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ5_Treugolniki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Нарисовать треугольники (стороны 10х10, диагональ корень 200) , только с помощью вложенных циклов (без IF ELSE)
+
+
+
+        }
+    }
+}
--- U22 Vlozhennie cikli/Program.cs
+++ U22 Vlozhennie cikli/Program.cs
@@ -10,6 +10,41 @@ namespace U22_Vlozhennie_cikli
     {
         static void Main(string[] args)
         {
+            //for (int i = 1; i <= 3; i++)
+            //{
+            //    Console.WriteLine("Cikl 1 Iteraciya " + i);
+
+            //    for (int j = 1; j <= 2; j++)
+            //    {
+            //        Console.WriteLine("\tCikl 2 Iteraciya " + j);
+
+            //        for (int k = 0; k <= 2; k++)
+            //        {
+            //            Console.WriteLine("\t\tCikl 3 Iteraciya " + k);
+            //        }
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+            Console.WriteLine("Vvedite visoty pryamougolnika");
+            int heigh = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vvedite shirinu pryamougolnika");
+            int width = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < heigh; j++)
+            {
+
+                for (int i = 0; i < width; i++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
 
         }
     }
