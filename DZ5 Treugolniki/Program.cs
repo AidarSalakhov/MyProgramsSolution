a7a5799cf46b403a8f07a9946c1b0de1871d446e
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-07 12:36:33 +0300

    Lol

--- DZ5 Treugolniki/Program.cs
+++ DZ5 Treugolniki/Program.cs
@@ -12,8 +12,22 @@ namespace DZ5_Treugolniki
         {
             //Нарисовать треугольники (стороны 10х10, диагональ корень 200) , только с помощью вложенных циклов (без IF ELSE)
 
+            int linelength = 1;
 
-
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                
+                for (int j = 0; j < linelength; j++)
+                {
+                    Console.Write("#");
+                    
+                }
+                Console.WriteLine();
+                linelength++;
+            }
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
