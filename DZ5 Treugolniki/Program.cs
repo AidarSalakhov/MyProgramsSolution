7ae188114a10822cf2631792fd5b712d5de83f48
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-07 14:18:10 +0300

    lol

--- DZ5 Treugolniki/Program.cs
+++ DZ5 Treugolniki/Program.cs
@@ -13,21 +13,88 @@ namespace DZ5_Treugolniki
             //Нарисовать треугольники (стороны 10х10, диагональ корень 200) , только с помощью вложенных циклов (без IF ELSE)
 
             int linelength = 1;
+            int linelength2 = 1;
 
             Console.WriteLine();
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i <= 9; i++)
             {
-                
                 for (int j = 0; j < linelength; j++)
                 {
                     Console.Write("#");
-                    
                 }
                 Console.WriteLine();
                 linelength++;
             }
             Console.WriteLine();
+
+
+
+            Console.WriteLine();
+            for (int k = 0; k <= 9; k++)
+            {
+                
+                for (int l = 1; l < linelength; l++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+                linelength--;
+            }
+            Console.WriteLine();
+
+
+
+            linelength = 10;
+
+
+
+            Console.WriteLine();
+            for (int h = 0; h <= 9; h++)
+            {
+
+                for (int g = 1; g < linelength; g++)
+                {
+                    Console.Write(" ");
+                }
+                linelength--;
+
+                for (int f = 0; f < linelength2; f++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+                linelength2++;
+            }
+            Console.WriteLine();
+
+
+
+
+            Console.WriteLine();
+            for (int h = 0; h <= 9; h++)
+            {
+
+                for (int g = 1; g < linelength; g++)
+                {
+                    Console.Write(" ");
+                }
+                linelength--;
+
+                for (int f = 0; f < linelength2; f++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+                linelength2++;
+            }
+            Console.WriteLine();
+
+
+
+
             Console.ReadLine();
+
+
         }
     }
 }
