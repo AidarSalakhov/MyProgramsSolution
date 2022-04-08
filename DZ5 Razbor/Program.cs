44856996a67d61fd9bf0a0f22e41d71e29430a19
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-08 12:31:52 +0300

    qwr

---
+++ DZ5 Razbor/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ5_Razbor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int heigh = 5;
+
+            for (int i = 0; i < heigh; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < heigh; i++)
+            {
+                for (int j = heigh; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < heigh; i++)
+            {
+                for (int j = heigh; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+
+
+            }
+            Console.WriteLine();
+
+            //for (int i = 0; i < heigh; i++)
+            //{
+            //    Console.SetCursorPosition(heigh - i, i + 1);
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+            for (int i = 0; i < heigh; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for (int j = heigh; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
