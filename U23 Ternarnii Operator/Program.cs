4343de08937e8686e0c2fddcc81d19d06a3bbcd6
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-08 13:15:46 +0300

    asd

---
+++ U23 Ternarnii Operator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U23_Ternarnii_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool accessAllowed;
+
+            //string storedPassword = "qwerty";
+            //string enteredPassword = Console.ReadLine();
+
+            //if (enteredPassword == storedPassword)
+            //{
+            //    accessAllowed = true;
+            //}
+            //else
+            //{
+            //    accessAllowed = false;
+            //}
+
+
+
+            //accessAllowed = enteredPassword == storedPassword ? true : false;
+
+            //Console.WriteLine(accessAllowed);
+
+            //Console.ReadLine();
+
+
+            int inputData = int.Parse(Console.ReadLine());
+
+            int outputData = (inputData < 0) ? 0 : inputData;
+
+            Console.WriteLine(outputData);
+
+            Console.ReadLine();
+
+        }
+    }
+}
