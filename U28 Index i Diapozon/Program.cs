b05e969c77947a14a2761eb78998fda5e12ecbb0
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-13 16:56:48 +0300

    dfg

--- U28 Index i Diapozon/Program.cs
+++ U28 Index i Diapozon/Program.cs
@@ -3,16 +3,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] myArray = { 1, 2, 3, 4, 5, 6 };
+        int[] myArray = { 2, 10, 5, 6, 77, 89 };
 
         int[] myArray2 = myArray[1..4]; //извлечение из другого массива, результат 2,3
 
-        int[] myArray2 = myArray[..4]; //извлечение из другого массива с начала, результат 1, 2,3
+        //int[] myArray2 = myArray[..4]; //извлечение из другого массива с начала, результат 1, 2,3
 
-        Console.WriteLine(myArray[^1]); //получить последний элемент массива с конца
+        Console.WriteLine(myArray[^2]); //получить последний элемент массива с конца
 
-        Index myIndex = ^2;
-        Console.WriteLine(myArray[myIndex]);
+        //Index myIndex = ^2;
+        //Console.WriteLine(myArray[myIndex]);
 
     }
 
