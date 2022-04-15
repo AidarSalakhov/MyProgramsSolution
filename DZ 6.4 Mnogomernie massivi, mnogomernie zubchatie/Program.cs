7f15c66cc664b4feb3c68fc60fd53ce97655037c
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-15 16:20:20 +0300

    цйук

---
+++ DZ 6.4 Mnogomernie massivi, mnogomernie zubchatie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_6._4_Mnogomernie_massivi__mnogomernie_zubchatie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Вывести на экран трёхмерный массив рандомных чисел
+            // 2. Вывести на экран четырёхмерыный массив рандомных чисел
+            // 3. Вывести на экран трёхмерный зубчатый массив
+            // 4. Вывести на экран зубчатый массив рандомной размерности с рандомными числами
+
+            
+
+
+
+
+
+        }
+    }
+}
