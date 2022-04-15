9adb54860eab31e3d480d425398253d4f1e1bdaa
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-15 16:41:19 +0300

    йцу

--- DZ 6.4 Mnogomernie massivi, mnogomernie zubchatie/Program.cs
+++ DZ 6.4 Mnogomernie massivi, mnogomernie zubchatie/Program.cs
@@ -15,11 +15,82 @@ namespace DZ_6._4_Mnogomernie_massivi__mnogomernie_zubchatie
             // 3. Вывести на экран трёхмерный зубчатый массив
             // 4. Вывести на экран зубчатый массив рандомной размерности с рандомными числами
 
-            
+            /*
+            Random random = new Random();
+
+            int[,,] myArray = new int[3,3,3];
+
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        myArray[i,j,k] = random.Next(100);
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        Console.Write(myArray[i,j,k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            */
 
 
 
+            /*
+            Random random = new Random();
 
+            int[,,,] myArray = new int[3, 3, 3, 3];
+
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        for (int l = 0; l < myArray.GetLength(3); l++)
+                        {
+                            myArray[i, j, k, l] = random.Next(100);
+                        }
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                Console.WriteLine("Book #" + (i +1) + "=======");
+                Console.WriteLine();
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.WriteLine("Page #" + (j+1));
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        for (int l = 0; l < myArray.GetLength(3); l++)
+                        {
+                            Console.Write(myArray[i, j, k, l] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            */
 
         }
     }
