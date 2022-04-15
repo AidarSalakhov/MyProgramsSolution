5c1d20681e0ce78cc13db90d84532f1000095613
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-15 09:47:52 +0300

    we

---
+++ DZ 6.2 Massivi/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_6._2_Massivi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int[,] myArray = new int[3,3];
+
+            //for (int i = 0; i < myArray.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < myArray.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(" Y: " + i + " X: " + j);
+            //        myArray[i,j] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+
+            //for (int i = 0; i < myArray.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < myArray.GetLength(1); j++)
+            //    {
+            //        Console.Write(myArray[i,j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            Random random = new Random();
+
+            int[,] myArray = new int[10, 5];
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    myArray[i, j] = random.Next(100);
+                }
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.Write(myArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
