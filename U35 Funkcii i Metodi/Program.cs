34f90d8b474e69a26391b2995b7d91afa8723cf3
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-18 15:50:55 +0300

    23

---
+++ U35 Funkcii i Metodi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U35_Funkcii_i_Metodi
+{
+    internal class Program
+    {
+        static int Sum(int value_1, int value_2) 
+        {
+            
+            return value_1 + value_2;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 5;
+            b = 5;
+            Console.WriteLine(Sum(a, b));
+        }
+    }
+}
