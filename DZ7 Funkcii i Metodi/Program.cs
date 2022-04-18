5d75827a2d5cd0d89de60800b57370735600a766
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-18 15:58:37 +0300

    234

---
+++ DZ7 Funkcii i Metodi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ7_Funkcii_i_Metodi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   1. Написать метод который выводит на экран строку.
+             *   Символы из которых состоит строка и их количество вводятся пользователем.
+             *   (например человек вводит "!" и "5" - вывестись должно !!!!!)
+             * 
+             *   2. Написать метод для поиска индекса элемента массива (тип элементов в массиве - int),
+             *   метод должен вернуть индекс первого найденного элемента (если он будет найден).
+             * 
+             */
+
+        }
+    }
+}
