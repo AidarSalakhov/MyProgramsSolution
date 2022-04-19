e878906266a5165277a8d4cde49d138faf0ec1c9
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-19 13:37:21 +0300

    Решено!

--- DZ0 160 000 euro v god/Program.cs
+++ DZ0 160 000 euro v god/Program.cs
@@ -16,7 +16,6 @@ namespace DZ0_160_000_euro_v_god
                 [2,4,5], k=8 []
                 [-2,-1,1,2], k=0 [-2,2]
             В заданных массивах найти 2 числа, которые в сумме дают k и вывести на экран эти числа.
-
             */
 
             Console.WriteLine("Введите длину одномерного массива:");
@@ -61,7 +60,11 @@ namespace DZ0_160_000_euro_v_god
             {
                 for (int j = 0; j < myArray.Length; j++)
                 {
-                    if (myArray[i] + myArray[j] == k)
+                    if (myArray[i] == myArray[j])
+                    {
+                        continue;
+                    }
+                    else if (myArray[i] + myArray[j] == k)
                     {
                         resultArray[0] = myArray[i];
                         resultArray[1] = myArray[j];
@@ -71,6 +74,5 @@ namespace DZ0_160_000_euro_v_god
             }
             return resultArray;
         }
-
     }
 }
