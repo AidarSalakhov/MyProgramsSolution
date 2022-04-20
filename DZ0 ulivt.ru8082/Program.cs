a486d16de3e89bf189bf101564471435992c7c67
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-20 16:36:30 +0300

    asd

---
+++ DZ0 ulivt.ru8082/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ0_ulivt.ru8082
+{
+    class Sums
+    {
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    internal class Program
+    {
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double Sum(double a, double b)
+        {
+            return a * b;
+        }
+
+        static void Main(string[] args)
+        {
+            int c=0;
+            Console.WriteLine(c);
+        }
+    }
+}
