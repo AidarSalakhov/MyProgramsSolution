ffb8017616b00a07529632ea52d0042753542179
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-26 12:52:22 +0300

    нш

---
+++ U40 Dva Voprosa NULL/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U40_Dva_Voprosa_NULL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "test";
+
+            //str = "1";
+
+            //Console.WriteLine(str ?? "Нет данных");
+
+
+            /*
+            string str = null;
+
+            string result = str ?? string.Empty;
+
+            Console.WriteLine("Количество символов в строке " + result.Length);
+
+            */
+
+
+            string str = null;
+
+            string result = str;
+
+            Console.WriteLine(result ??= "Нет данных");
+
+        }
+    }
+}
---
+++ U40 NULL/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U40_NULL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "test";
+
+            //str = "1";
+
+            //Console.WriteLine(str ?? "Нет данных");
+
+
+            /*
+            string str = null;
+
+            string result = str ?? string.Empty;
+
+            Console.WriteLine("Количество символов в строке " + result.Length);
+
+            */
+
+
+            /*
+            string str = null;
+
+            string result = str ??= "xyi";
+
+            Console.WriteLine("Количество символов в строке " + result);
+            */
+
+            string str = "test";
+
+            str = null;
+
+            Console.WriteLine(str ??= "Нет данных");
+
+        }
+    }
+}
