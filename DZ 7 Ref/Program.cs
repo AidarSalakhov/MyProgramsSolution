28b2f0fd926acbbc8c1e641f3fe7e28a66bd78f4
Aidar Salakhov <adr-sal@yandex.ru>
2022-04-27 15:51:17 +0300

    wqe

--- DZ 7 Ref/Program.cs
+++ DZ 7 Ref/Program.cs
@@ -8,6 +8,35 @@ namespace DZ_7_Ref
 {
     internal class Program
     {
+        static int[] ResizeArray(ref int[] myArray, ref int arraySize)
+        {
+            myArray = new int [arraySize];
+
+            return myArray;
+        }
+
+        static int[] RandomizeArray(int[] myArray)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < myArray.Length; i++)
+                myArray[i] = random.Next(100);
+
+            return myArray;
+        }
+
+        static void PrintlineArray(int[] myArray)
+        {
+            Console.Write("Ваш массив myArray[] = ");
+
+            for (int i = 0; i < myArray.Length; i++)
+                Console.Write("[" + myArray[i] + "]");
+
+            Console.WriteLine();
+        }
+
+
+
         static void Main(string[] args)
         {
             /*
@@ -25,6 +54,30 @@ namespace DZ_7_Ref
             последнего элемента массива и элемента по указанному индексу.
 
             */
+
+
+
+                Console.WriteLine("Введите размер вашего массива:");
+
+                int arraySize = int.Parse(Console.ReadLine());
+
+                int[] myArray = new int[arraySize];
+
+                RandomizeArray(myArray);
+
+                PrintlineArray(myArray);
+
+                Console.WriteLine("Измените размер вашего массива:");
+
+                arraySize = int.Parse(Console.ReadLine());
+
+                ResizeArray(ref myArray, ref arraySize);
+
+                RandomizeArray(myArray);
+
+                PrintlineArray(myArray);
+
+
         }
     }
 }
