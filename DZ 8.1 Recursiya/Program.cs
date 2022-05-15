6962341e1a0bd2599191ebb0345e54be71c39743
AidarSalakhov <adr-sal@yandex.ru>
2022-05-15 20:54:11 +0300

    ПВ

--- DZ 8.1 Recursiya/Program.cs
+++ DZ 8.1 Recursiya/Program.cs
@@ -13,8 +13,72 @@ namespace DZ_8._1_Recursiya
         // 2. Найти сумму элементов массива с помощью рекурсии.
         // 3. Найти сумму цифр числа с помощью рекурсии. 561 = 12
 
+
+        static void PrintArray(int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+
+        static void PrintArrayWithRecursion(int[] myArray, int i = 0)
+        {
+
+            Console.WriteLine(myArray[i]);
+
+            i++;
+
+            if (i >= myArray.Length)
+            {
+                return;
+            }
+
+            PrintArrayWithRecursion(myArray, i);
+        }
+
+        //static int SumArrayValueWithRecursion(int[] myArray,  int sum = 0, int i = 0)
+        //{
+        //    sum += myArray[i];
+
+        //    i++;
+
+        //    if (i >= myArray.Length)
+        //    {
+        //        return sum;
+        //    }
+
+        //    SumArrayValueWithRecursion(myArray, sum, i);
+
+        //    return sum;
+        //}
+
+
+        static int SumArrayValueWithRecursion(int[] myArray, int sum = 0, int i = 0)
+        {
+            int outsum = 0;
+
+            sum += myArray[i];
+
+            i++;
+
+            if (i >= myArray.Length)
+            {
+                outsum = sum;
+            }
+
+            SumArrayValueWithRecursion(myArray, sum, i);
+
+            return outsum;
+        }
+
         static void Main(string[] args)
         {
+            int[] myArray = { 1, 2, 3, 4, 5, 6, 7 };
+
+            
+            Console.WriteLine(SumArrayValueWithRecursion(myArray));
+
         }
     }
 }
