79c9c6fbdfb5e02f7315abf10b6a75ac0af3a9fc
AidarSalakhov <adr-sal@yandex.ru>
2022-05-28 18:19:22 +0300

    Погружаемся в классы

---
+++ U55 Class/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U55_Class
+{
+    enum Color
+    {
+        Black,
+        White
+    }
+
+    class Person
+    {
+        public const string type = "Person";
+
+        public int id = 0;
+        public string name = "";
+        public int? age = null;
+        public string city = "";
+        public Color color;
+
+        public void Print()
+        {
+            Console.WriteLine($"{type} with id {id}, named {name}, with age {age}, living in {city}, color {color}");
+        }
+    }
+}
--- U55 Class/Program.cs
+++ U55 Class/Program.cs
@@ -6,25 +6,60 @@ using System.Threading.Tasks;
 
 namespace U55_Class
 {
-    class Person
+    class Student
     {
-        public const string type = "Person";
-
-        public static int id;
-        public static string name;
-        public static int age;
-        public static int city;
-
-        public static void Print()
-        {
-            Console.WriteLine($"{type} with id {id}, named {name}, with {age}, living in {city}");
-        }
+        public Guid id;
+        public string name;
+        public int age;
     }
 
     internal class Program
     {
+        static Student GetStudent()
+        {
+            Student student = new Student();
+
+            student.id = Guid.NewGuid();
+            student.name = "Vitaliy";
+            student.age = 19;
+
+            return student;
+        }
+
+        static void Print(Student student)
+        {
+            Console.WriteLine(student.id);
+            Console.WriteLine(student.age);
+            Console.WriteLine(student.name);
+        }
+
         static void Main(string[] args)
         {
+            var student = GetStudent();
+            Print(student);
+            
+            
+            
+            //var person = new Person();
+
+            //person.id = 1;
+            //person.name = "ivan";
+            //person.age = 37;
+            //person.city = "Moscow";
+            //person.color = Color.White;
+
+            //person.Print();
+
+            //var person2 = new Person();
+
+            //person2.id = 2;
+            //person2.name = "alexei";
+            //person2.age = 39;
+            //person2.city = "London";
+
+            //person2.Print();
+
+
         }
     }
 }
