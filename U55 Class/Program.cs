a01261e11de493e0f5f129629adf62b2fbd0facf
AidarSalakhov <adr-sal@yandex.ru>
2022-05-28 16:41:36 +0300

    Тестирую классы

---
+++ U55 Class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U55_Class
+{
+    class Person
+    {
+        public const string type = "Person";
+
+        public static int id;
+        public static string name;
+        public static int age;
+        public static int city;
+
+        public static void Print()
+        {
+            Console.WriteLine($"{type} with id {id}, named {name}, with {age}, living in {city}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
