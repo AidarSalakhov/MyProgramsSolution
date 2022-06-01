7d706d2a16329aa82e525decee487707a82a8faa
Aidar Salakhov <adr-sal@yandex.ru>
2022-06-01 10:44:06 +0300

    Инкапсуляция изучена

---
+++ U58 Incapsulyaciya/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U58_Incapsulyaciya
+{
+    class Gun
+{
+        private bool isLoaded;
+
+        private void Reload()
+        {
+            Console.WriteLine("Заряжаю...");
+
+            isLoaded = true;
+
+            Console.WriteLine("Заряжено!");
+        }
+
+        public void Shoot()
+        {
+            if (!isLoaded)
+            {
+                Console.WriteLine("Не заряжено!");
+
+                Reload();
+            }
+
+            Console.WriteLine("Пыщь-Пыщь");
+
+            isLoaded= false;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun();
+
+            gun.Shoot();
+        }
+    }
+}
