15a0d40ba959512608b722c44dd197757b254c45
Aidar Salakhov <adr-sal@yandex.ru>
2022-06-01 13:32:00 +0300

    Изучаю коснтрукторы класса

---
+++ U59 Konstruktor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U59_Konstruktor
+{
+    class Gun
+    {
+        public Gun(bool isLoaded)
+        {
+            //this.isLoaded = isLoaded;
+            _isLoaded = isLoaded;
+        }
+
+        private bool _isLoaded;
+
+        private void Reload()
+        {
+            Console.WriteLine("Заряжаю...");
+
+            _isLoaded = true;
+
+            Console.WriteLine("Заряжено!");
+        }
+
+        public void Shoot()
+        {
+            if (!_isLoaded)
+            {
+                Console.WriteLine("Не заряжено!");
+
+                Reload();
+            }
+
+            Console.WriteLine("Пыщь-Пыщь");
+
+            _isLoaded = false;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun(isLoaded: true);
+
+            gun.Shoot();
+        }
+    }
+}
---
+++ U59-2 Konstruktor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U59_2_Konstruktor
+{
+    class Point
+    {
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        private int _x;
+        private int _y;
+
+        public void Print()
+        {
+            Console.WriteLine($"X: {_x}, Y: {_y}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point(6, 12);
+
+            point.Print();
+        }
+    }
+}
