12ec29b6971a1c805310ba81929fb2b2628b38da
Aidar Salakhov <adr-sal@yandex.ru>
2022-06-06 16:21:56 +0300

    Static

---
+++ U62 Static/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U62_Static
+{
+    class MyClass
+    {
+        //public int a;
+
+        public static int b;
+
+        public void SetB(int b)
+        {
+            MyClass.b = b;
+        }
+
+        public void PrintB()
+        {
+            Console.WriteLine(b);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //MyClass myClass1 = new MyClass();
+            //myClass1.a = 44;
+
+            //MyClass myClass2 = new MyClass();
+            //myClass2.a = 22;
+
+            //MyClass.b = 6;
+
+            MyClass myClass1 = new MyClass();
+            myClass1.SetB(1);
+
+            MyClass MyClass2 = new MyClass();
+            MyClass2.PrintB();
+
+        }
+    }
+}
